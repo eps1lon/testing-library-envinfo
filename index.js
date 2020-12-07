081caca1bde5b997bebae5c04f5724c6fd072f13
eps1lon <silbermann.sebastian@gmail.com>
2020-12-07 18:56:33 +0100

    Remove envinfo as a dependency

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 #!/usr/bin/env node
-const envinfo = require("envinfo");
+const { spawnSync } = require("child_process");
 
-envinfo
-  .run(
-    {
-      npmPackages: `{${[
-        "@testing-library/*",
-        // important transitive dependencies
-        "aria-query",
-        "dom-accessibility-api",
-        // Libraries tested by @testing-library/*
-        "react",
-        "react-dom",
-        // testing environment
-        "jsdom",
-        // test runner
-        "jest",
-      ]}}`,
-      Binaries: ["Node", "Yarn", "npm"],
-      System: ["OS"],
-    },
-    {
-      duplicates: true,
-      // include transitive dependencies and important packages that are transitive dependencies (e.g. `jsdom` is usually a transitive dependency inside jest)
-      fullTree: true,
-      markdown: true,
-      showNotFound: true,
-    }
-  )
-  .then((env) => console.log(env));
+const options = {
+  npmPackages: `{${[
+    "@testing-library/*",
+    // important transitive dependencies
+    "aria-query",
+    "dom-accessibility-api",
+    // Libraries tested by @testing-library/*
+    "react",
+    "react-dom",
+    // testing environment
+    "jsdom",
+    // test runner
+    "jest",
+  ]}}`,
+  Binaries: ["Node", "Yarn", "npm"],
+  System: ["OS"],
+};
+
+const { status } = spawnSync(
+  "npx",
+  [
+    "envinfo",
+    "--raw",
+    `${JSON.stringify(options)}`,
+    "--duplicates",
+    "--fullTree",
+    "--showNotFound",
+  ],
+  {
+    stdio: "inherit",
+  }
+);
+
+if (status !== null) {
+  process.exit(status);
+}
